584864c9c1628413b98e8b686095ec02bbb70efc
Nathan Baltzell <baltzell@gmx.com>
2022-12-23 19:04:21 -0500

    add ccdb plotter

--- bin/ccdb-plot.py
+++ bin/ccdb-plot.py
@@ -9,40 +9,35 @@ import sqlalchemy
 import ROOT
 
 def get_row(table, slco):
-  for row in data_table:
-    good = True
-    for column,value in enumerate(args.slco):
-      if int(row[column]) != value:
-        good = False
-        break
-    if good:
-      return row
-  return None
+    for row in data_table:
+        good = True
+        for column,value in enumerate(args.slco):
+            if int(row[column]) != value:
+                good = False
+                break
+        if good:
+            return row
+    return None
 
 def get_cell(table, slco, column_index):
-  row = get_row(table, slco)
-  if row is not None:
-    return float(row[column_index])
-  return None
+    row = get_row(table, slco)
+    if row is not None:
+        return float(row[column_index])
+    return None
 
 cli = argparse.ArgumentParser(description='Plot one variable from a CCDB table with 3 or 4 leading columns.')
-
 cli.add_argument('-min', metavar='#', help='minimum run number', type=int, required=True)
 cli.add_argument('-max', metavar='#', help='maximum run number', type=int, required=True)
 cli.add_argument('-table', help='table name', type=str, required=True)
 cli.add_argument('-variable', help='variable name', type=str, required=True)
 cli.add_argument('-slco', metavar='#/#/#[/#]', help='leading column values, e.g. sector/layer/component[/order]', required=True)
-cli.add_argument('-ignorezeros', help='ignore zero values', default=False, action='store_true')
+cli.add_argument('-ignore', help='value to discard from plot (repeatable)', default=[], type=float, action='append')
 cli.add_argument('-variation', help='variation name (default=default)', type=str, default='default')
 cli.add_argument('-timestamp', metavar='MM/DD/YYYY[-HH:mm:ss]', help='timestamp (default=now)', type=str, default=None)
 cli.add_argument('-batch', help='do not open ROOT canvas', default=False, action='store_true')
+cli.add_argument('-dump', help='print table to stdout', default=False, action='store_true')
 cli.add_argument('-v', help='verbose mode', default=False, action='store_true')
 args = cli.parse_args(sys.argv[1:])
-
-try:
-    args.slco = [int(x) for x in args.slco.split('/')]
-except:
-    cli.error('Invalid s/l/c[/o]: '+args.slco)
 
 if args.max < args.min:
     cli.error('Invalid run range, min>max:  min=%d  max=%d.' % (args.min, args.max))
@@ -56,19 +51,29 @@ if args.timestamp is not None:
         except ValueError:
             cli.error('Invalid timestamp:  '+args.timestamp)
 
+try:
+    args.slco = [int(x) for x in args.slco.split('/')]
+except:
+    cli.error('Invalid s/l/c[/o]: '+args.slco)
+
 provider = ccdb.AlchemyProvider()
 provider.connect(os.getenv('CCDB_CONNECTION'))
 
-column_names = [x.name for x in provider.get_type_table(args.table).columns]
-column_index = column_names.index(args.variable)
+try:
+    column_names = [x.name for x in provider.get_type_table(args.table).columns]
+except ccdb.errors.TypeTableNotFound:
+    cli.error('Invalid table name:  '+args.table)
+try:
+    column_index = column_names.index(args.variable)
+except ValueError:
+    cli.error('Invalid variable name:  '+args.variable)
 if column_index < 0:
     cli.error('Invalid variable name:  '+args.variable)
-
-graphs = [ROOT.TGraph().SetName('g%d'%x) for x in args.slco]
 
 graph = ROOT.TGraph()
 
 for run in range(args.min, args.max+1):
+
     if args.v:
         print('Retrieving run #%d ...'%run)
     try:
@@ -78,16 +83,19 @@ for run in range(args.min, args.max+1):
     except ccdb.errors.DirectoryNotFound:
         cli.error('Invalid table or variation:  table=%s  variation=%s.' % (args.table, args.variation))
     except sqlalchemy.orm.exc.NoResultFound:
+        print('Warning:  no data found for run ',run)
         continue
 
     data_table = assignment.constant_set.data_table
     value = get_cell(data_table, args.slco, column_index)
 
-    if value is not None:
-        if not args.ignorezeros or value!=0:
-            graph.AddPoint(run,value)
+    if value is not None and value not in args.ignore:
+        graph.AddPoint(run,value)
+        if args.dump:
+            print(run,value)
 
 if not args.batch:
+    graph.SetLineWidth(2)
     graph.Draw()
     input()
 
