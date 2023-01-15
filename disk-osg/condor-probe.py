e2cfd9d9adc89055a8a3ba5a3cb8a02c3c18f5e6
Nathan Baltzell <baltzell@gmx.com>
2023-01-15 14:38:50 -0500

    cleanup job matching

--- disk-osg/condor-probe.py
+++ disk-osg/condor-probe.py
@@ -211,11 +211,6 @@ def condor_calc_wallhr(job):
 ###########################################################
 ###########################################################
 
-def condor_yield(args):
-  for condor_id,job in condor_data.items():
-    if condor_match(job, args):
-      yield (condor_id, job)
-
 class Matcher():
   def __init__(self, values):
     self.values = []
@@ -230,7 +225,7 @@ class Matcher():
       return False
     if str(value) in self.values:
       return True
-    return len(self.values)==0 
+    return len(self.values)==0 or value is None
   def pattern_matches(self, value):
     for v in self.antivalues:
       if v.find(str(value)) >= 0:
@@ -238,66 +233,70 @@ class Matcher():
     for v in self.values:
       if v.find(str(value)) >= 0:
         return True
-    return len(self.values)==0
+    return len(self.values)==0 or value is None
 
-condor_matcher = None
-site_matcher = None
-gemc_matcher = None
-user_matcher = None
-exit_matcher = None
-gen_matcher = None
-host_matcher = None
-def condor_match(job, args):
-  ''' Apply job constraints, on top of those condor knows about'''
-  global condor_matcher
-  if condor_matcher is None:
-    global site_matcher
-    global gemc_matcher
-    global user_matcher
-    global exit_matcher
-    global gen_matcher
-    global host_matcher
-    condor_matcher = Matcher(args.condor)
-    site_matcher = Matcher(args.site)
-    gemc_matcher = Matcher(args.gemc)
-    user_matcher = Matcher(args.user)
-    exit_matcher = Matcher(args.exit)
-    gen_matcher = Matcher(args.generator)
-    host_matcher = Matcher(args.host)
-  if job.get('condor') is None:
-    return False
-  if not condor_matcher.matches(job.get('condor').split('.').pop(0)):
-    return False
-  if not gemc_matcher.matches(job.get('gemc')):
-    return False
-  if not user_matcher.matches(job.get('user')):
-    return False
-  if not site_matcher.pattern_matches(job.get('MATCH_GLIDEIN_Site')):
-    return False
-  if not host_matcher.pattern_matches(job.get('LastRemoteHost')):
-    return False
-  if not gen_matcher.matches(job.get('generator')):
-    return False
-  if args.noexit:
-    if job.get('ExitCode') is not None:
+class CondorMatcher(Matcher):
+  def __init__(self, values, key):
+    self.key = key
+    super().__init__(values)
+  def matches(self, job):
+    return super().matches(job.get(self.key))
+  def pattern_matches(self, job):
+    return super().pattern_matches(job.get(self.key))
+
+class CondorMatchers():
+  def __init__(self, args):
+    self.args = args
+    self.exit = CondorMatcher(args.exit, 'ExitCode')
+    self.gemc = CondorMatcher(args.gemc, 'gemc')
+    self.user = CondorMatcher(args.user, 'user')
+    self.site = CondorMatcher(args.site, 'MATCH_GLIDEIN_Site')
+    self.host = CondorMatcher(args.host, 'LastRemoteHost')
+    self.condor = CondorMatcher(args.condor, 'condor')
+    self.generator = CondorMatcher(args.generator, 'generator')
+  def matches(self, job):
+    if args.noexit and job.get('ExitCode') is not None:
       return False
-  elif not exit_matcher.matches(job.get('ExitCode')):
-    return False
-  if args.plot is False:
-    if args.idle and job_states.get(job['JobStatus']) != 'I':
+    if job.get('condor') is None:
       return False
-    if args.completed and job_states.get(job['JobStatus']) != 'C':
+    if not super(CondorMatcher, self.condor).matches(job.get('condor').split('.').pop(0)):
       return False
-    if args.running and job_states.get(job['JobStatus']) != 'R':
+    if not self.gemc.matches(job):
       return False
-    if args.held and job_states.get(job['JobStatus']) != 'H':
+    if not self.user.matches(job):
       return False
-  try:
-    if int(job['CompletionDate']) > int(args.end.timestamp()):
+    if not self.site.pattern_matches(job):
       return False
-  except:
-    pass
-  return True
+    if not self.host.pattern_matches(job):
+      return False
+    if not self.generator.matches(job):
+      return False
+    elif not self.exit.matches(job):
+      return False
+    if self.args.plot is False:
+      if self.args.idle and job_states.get(job['JobStatus']) != 'I':
+        return False
+      if self.args.completed and job_states.get(job['JobStatus']) != 'C':
+        return False
+      if self.args.running and job_states.get(job['JobStatus']) != 'R':
+        return False
+      if self.args.held and job_states.get(job['JobStatus']) != 'H':
+        return False
+    try:
+      if int(job['CompletionDate']) > int(self.args.end.timestamp()):
+        return False
+    except:
+      pass
+    return True
+
+def condor_yield(args):
+  cm = CondorMatchers(args)
+  for condor_id,job in condor_data.items():
+    if cm.matches(job):
+      yield (condor_id, job)
+
+###########################################################
+###########################################################
 
 def get_status_key(job):
   if job_states[job['JobStatus']] == 'H':
