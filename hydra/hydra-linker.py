8704939ef6fd3f693abab72ac1cbd667052e4bd8
baltzell <baltzell@gmx.com>
2023-03-18 15:37:11 -0400

    remove old linker, fix lost changes

--- hydra/hydra-linker.py
+++ hydra/hydra-linker.py
@@ -1,13 +1,46 @@
 #!/usr/bin/env python3
-import re,os,time,glob,datetime
+import re,os,time,glob,subprocess,datetime
 
-input_dir = '/home/clasrun/CLAS12MON/output'
+dryrun = False
+minimum_runno = 17762
+minimum_age_seconds = 120
+input_dir = '/local/baltzell/hydra/images'
 output_dir = '/local/hydra/input'
-blacklist_file = '/home/baltzell/hydra-blacklist.txt'
+blacklist_file = '/local/baltzell/hydra/blacklist.txt'
 indir_regex = '^clas12mon_(\d+)_(\d+-\d+-\d+_\d+\.\d+\.\d+_[APM]+)$'
 time_format = '%m-%d-%Y_%H.%M.%S_%p'
 
-def find():
+def read_blacklist(filename):
+  blacklist = {}
+  with open(filename,'r') as f:
+    for line in f.readlines():
+      try:
+        runno,timestamp = line.strip().split()
+        runno = int(runno)
+        timestamp = datetime.datetime.strptime(timestamp, time_format)
+      except:
+        continue
+      if runno not in blacklist:
+        blacklist[runno] = []
+      blacklist[runno].append(timestamp)
+      blacklist[runno] = sorted(blacklist[runno])
+  return blacklist
+
+def update_blacklist(filename, additions):
+  with open(filename,'a') as f:
+    for x in additions:
+      f.write(x+'\n')
+
+def assign_chunks(data, blacklist):
+  for runno in data.keys():
+    chunks = []
+    if runno in blacklist:
+      chunks.extend(blacklist.get(runno))
+    chunks.extend(data.get(runno).keys())
+    for chunk,timestamp in enumerate(sorted(chunks)):
+      data[runno][timestamp]['chunk'] = chunk
+
+def find(blacklist):
   data = {}
   for dirpath,dirnames,filenames in os.walk(input_dir):
     for dirname in dirnames:
@@ -15,47 +48,59 @@ def find():
       m = re.match(indir_regex, dirname)
       if m is None:
         continue
-      if time.time() - os.path.getmtime(fulldirname) < 120:
+      if time.time() - os.path.getmtime(fulldirname) < minimum_age_seconds:
         continue
-      runno = m.group(1)
+      runno = int(m.group(1))
       timestamp = m.group(2)
+      if runno < minimum_runno:
+        continue
       try:
         timestamp = datetime.datetime.strptime(timestamp, time_format)
       except ValueError:
         continue
       if runno not in data:
         data[runno] = {}
-      data[runno][timestamp] = fulldirname
+      data[runno][timestamp] = {'dirpath':fulldirname}
     break
+  assign_chunks(data, blacklist)
   return data
 
 def link(data):
-  n_additions = 0
-  with open(blacklist_file,'r') as bfile:
-    blacklist = [x.strip() for x in bfile.readlines()]
-  with open(blacklist_file,'a') as bfile:
-    for runno in sorted(data.keys()):
-      for chunk,timestamp in enumerate(sorted(data[runno].keys())):
-        stub = runno + ' ' + str(chunk)
-        if stub in blacklist:
+  additions = []
+  for runno in sorted(data.keys()):
+    for timestamp in data.get(runno).keys():
+      if runno in blacklist:
+        if timestamp in blacklist.get(runno):
           continue
-        bfile.write(stub+'\n')
-        idir = data[runno][timestamp]
-        odir = output_dir + '/' + runno
+      stub = '%d %s'%(runno,datetime.datetime.strftime(timestamp, time_format))
+      additions.append(stub)
+      idir = data[runno][timestamp]['dirpath']
+      chunk = data[runno][timestamp]['chunk']
+      odir = output_dir + '/' + str(runno)
+      if dryrun:
+        print(stub)
+      else:
         os.makedirs(odir, exist_ok=True)
         os.chmod(odir, 0o777)
-        n_additions += 1
-        for png in glob.glob(idir+'/*.png'):
-          png = os.path.basename(png)
-          if re.match('.*\d+-\d+-\d+_\d+\.\d+\.\d+.*', png) is not None:
-            continue
-          new_png = png[:-4] + '_%.4d.png'%chunk
-          src = idir + '/' + png
-          dst = odir + '/' + new_png
+      for png in glob.glob(idir+'/*.png'):
+        png = os.path.basename(png)
+        if re.match('.*\d+-\d+-\d+_\d+\.\d+\.\d+.*', png) is not None:
+          continue
+        new_png = png[:-4] + '_%.4d.png'%chunk
+        src = idir + '/' + png
+        dst = odir + '/' + new_png
+        if dryrun:
+          print(src+'  ->  '+dst)
+        elif not os.path.exists(dst):
           os.symlink(src, dst)
-  return n_additions
+  return additions
 
+print('Starting hydra-linker ...')
 while True:
-  print('Added %d runchunks.'%link(find()))
+  blacklist = read_blacklist(blacklist_file)
+  additions = link(find(blacklist))
+  update_blacklist(blacklist_file, additions)
+  if len(additions) > 0:
+    print('Added %d runchunks.'%len(additions))
   time.sleep(60)
 
