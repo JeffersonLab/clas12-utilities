de14bde7f20a3a52eccd1c5bdddf9cd9a51ca92b
baltzell <baltzell@gmx.com>
2023-03-18 15:32:35 -0400

    move hydra image/log, add procServ

---
+++ hydra/procServ-linker.sh
@@ -0,0 +1,2 @@
+procServ -n hydra-linker-2 -i^D^C --logfile /local/baltzell/hydra/logs/hydra-linker.log --logstamp -c /home/baltzell/clas12-utilities 20001 ./hydra/hydra-linker-2.py
+
---
+++ hydra/procServ-mon12.sh
@@ -0,0 +1,2 @@
+procServ -n hydra-mon12 -i^D^C --logfile /local/baltzell/hydra/logs/mon12.log --logstamp -c /home/baltzell/mon12 20002 ./bin/mon12_hydra
+
