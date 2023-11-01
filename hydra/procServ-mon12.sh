fc5ccfd512c1bf18d2deb4694797d2705ccb2353
baltzell <baltzell@gmx.com>
2023-11-01 09:34:22 -0400

    use clondb

--- hydra/procServ-mon12.sh
+++ hydra/procServ-mon12.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+export CCDB_CONNECTION=mysql://clas12reader@clondb1.jlab.org/clas12
 procServ -n hydra-mon12 -i^D^C --logfile /local/baltzell/hydra/logs/mon12.log --logstamp -c /home/baltzell/mon12 20002 ./bin/mon12_hydra
 
