5a16746966fb9a159d7a11b52b2beb66fa6b67eb
baltzell <baltzell@gmx.com>
2023-11-01 09:43:36 -0400

    get old images for hydra

--- hydra/hydra-linker.py
+++ hydra/hydra-linker.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 import re,os,time,glob,subprocess,datetime
 
-dryrun = False
-minimum_runno = 18338
-minimum_age_seconds = 60
+dryrun = False            # do not link any images, just print
+chunk_offset = 0          # assign chunk numbers with an offset
+image_startswith = None   # limit to filenames starting with this
+ignore_blacklist = False  # ignore the blacklist file and don't update it
+
+maximum_runno = 100000    # ignore run numbers larger than this
+minimum_runno = 18338     # ignore run numbers smaller than this
+minimum_age_seconds = 60  # minimum age for considering images
+
 input_dir = '/local/baltzell/hydra/images'
 output_dir = '/local/hydra/input'
 blacklist_file = '/local/baltzell/hydra/blacklist.txt'
@@ -38,7 +44,7 @@ def assign_chunks(data, blacklist):
       chunks.extend(blacklist.get(runno))
     chunks.extend(data.get(runno).keys())
     for chunk,timestamp in enumerate(sorted(chunks)):
-      data[runno][timestamp]['chunk'] = chunk
+      data[runno][timestamp]['chunk'] = chunk + chunk_offset
 
 def find(blacklist):
   data = {}
@@ -52,7 +58,7 @@ def find(blacklist):
         continue
       runno = int(m.group(1))
       timestamp = m.group(2)
-      if runno < minimum_runno:
+      if runno < minimum_runno or runno > maximum_runno:
         continue
       try:
         timestamp = datetime.datetime.strptime(timestamp, time_format)
@@ -83,6 +89,9 @@ def link(data):
         os.makedirs(odir, exist_ok=True)
         os.chmod(odir, 0o777)
       for png in glob.glob(idir+'/*.png'):
+        if image_startswith is not None:
+          if not png.split('/').pop().startswith(image_startswith):
+            continue
         png = os.path.basename(png)
         if re.match('.*\d+-\d+-\d+_\d+\.\d+\.\d+.*', png) is not None:
           continue
@@ -93,13 +102,17 @@ def link(data):
           print(src+'  ->  '+dst)
         elif not os.path.exists(dst):
           os.symlink(src, dst)
+          print(src+'  ->  '+dst)
   return additions
 
 print('Starting hydra-linker ...')
 while True:
-  blacklist = read_blacklist(blacklist_file)
+  blacklist = {}
+  if not ignore_blacklist:
+    blacklist = read_blacklist(blacklist_file)
   additions = link(find(blacklist))
-  update_blacklist(blacklist_file, additions)
+  if not ignore_blacklist:
+    update_blacklist(blacklist_file, additions)
   if len(additions) > 0:
     t = datetime.datetime.strftime(datetime.datetime.now(), '%m/%d/%y %H:%M:%S')
     print('%s: Added %d runchunks.'%(t,len(additions)))
