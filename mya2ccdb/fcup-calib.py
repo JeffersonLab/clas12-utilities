f529edd6e41049ecca0ae0b3e136271e33ca79ab
Nathan Baltzell <baltzell@gmx.com>
2023-12-18 10:22:19 -0500

    add plot of attenuation

--- mya2ccdb/fcup-calib.py
+++ mya2ccdb/fcup-calib.py
@@ -255,14 +255,17 @@ def plot(path):
     data = pd.read_csv(path,sep='\s+',header=None)
     data = pd.DataFrame(data)
     import matplotlib.pyplot as plt
-    fig, ax = plt.subplots()
-    ax.set_ylabel('Faraday Cup Offset  (Hz)')
-    ax.set_xlabel('Run Number')
+    fig, (ax1,ax2) = plt.subplots(1,2)
+    fig.suptitle('Faraday Cup Calibrations')
+    ax2.set_ylabel('Faraday Cup Attenuation')
+    ax1.set_ylabel('Faraday Cup Offset  (Hz)')
+    ax1.set_xlabel('Run Number')
     x = data[0]
     y = data[2]
     ey = data[3]
-    plt.plot(x, y,'r',marker='.',linestyle='')
-    plt.errorbar(x, y, xerr=None, yerr=ey, fmt='r',marker='.',linestyle='')
+    ax1.errorbar(x, y, xerr=None, yerr=ey, fmt='r',marker='.',linestyle='')
+    y = data[4]
+    ax2.plot(x, y,'r',marker='.',linestyle='')
     print('Close plot window to quit.')
     plt.show()
 
