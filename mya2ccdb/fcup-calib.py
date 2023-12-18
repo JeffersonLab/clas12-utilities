56ec2ce4484ab4378105cee63b4ea4a4493f0539
Nathan Baltzell <baltzell@gmx.com>
2023-12-18 17:19:54 -0500

    cleanup

--- mya2ccdb/fcup-calib.py
+++ mya2ccdb/fcup-calib.py
@@ -76,8 +76,6 @@ class RCDB:
         except AttributeError:
             print('ERROR:  %s unavailable for run %d in RCDB.'%(name, run))
             return None
-    def get_event_count(self, run):
-        return self.get_condition(run, 'event_count')
     def get_timespan(self, first_run, last_run):
         start = self.get_condition(first_run, 'run_start_time')
         end = self.get_condition(last_run, 'run_end_time')
@@ -87,6 +85,8 @@ class RCDB:
             print('ERROR:  Invalid range:  %s (%d) -> %s (%d)'%(start,first_run,end,last_run))
             return None
         return start,end
+    def get_event_count(self, run):
+        return self.get_condition(run, 'event_count')
     def cleanup(self):
         try:
             self.db.disconnect()
@@ -103,6 +103,8 @@ class FcupTable:
         self.offset = offset
         self.offset_rms = offset_rms
         self.atten = atten
+        self.stops = []
+        self.energies = []
         self.span = None
     def __str__(self):
         s = '# %s-%s %s %d-%d\n'%(os.path.basename(__file__),version,timestamp,self.runmin,self.runmax)
@@ -121,25 +123,34 @@ class FcupTable:
             f.write(str(self))
     def get_cmd(self):
         return 'ccdb -c $CCDB_CONNECTION add -r %d-%d %s'%(self.runmin,self.runmax,os.path.basename(self.filename))
-    def set_atten(self, run, stopper, energy):
-        if stopper < stopper_threshold:
-            self.atten = 1.0
-        else:
-            if run>=12444 and run<=12853:
+    def set_atten(self):
+        if len(self.stops)>0 and len(self.energies)>0:
+            if self.stops[0] < stopper_threshold:
+                self.atten = 1.0
+            elif self.runmin>=12444 and self.runmin<=12853:
                 # fix bad beam energy from MCC:
-                energy = 10405
-            elif run>=18305 and run<=19131:
+                self.atten = 9.6930
+            elif self.runmin>=18305 and self.runmin<=19131:
                 # RG-D with Faraday cup issues:
                 self.atten = 5.79985
-                return
-            for e,a in attenuations.items():
-                if math.fabs(e-energy) < energy_tolerance_MeV:
-                    self.atten = a
-                    return
-            print('ERROR:  Unknown beam energy:  %.2f'%energy)
+            else:
+                for e,a in attenuations.items():
+                    if math.fabs(e-self.energies[0]) < energy_tolerance_MeV:
+                        self.atten = a
+                        return
+                print('ERROR:  Unknown beam energy:  %.2f'%self.energies[0])
+    def check(self):
+        if len(self.energies) > 1:
+            print('ERROR:  found multiple beam energies for %d:  %s'%(self.runmin,' '.join([str(x) for x in self.energies])))
+        if len(self.stops) > 1:
+            print('ERROR:  found multiple beam stopper positions for %d:  %s'%(self.runmin,' '.join([str(x) for x in self.stops])))
+        if self.noffsets < 5:
+            print('ERROR:  only %d points found for fcup offset for %d-%d.'%(self.noffsets,self.runmin,self.runmax))
+        elif self.noffsets < 10:
+            print('WARNING:  only %d points found for fcup offset for %d-%d.'%(self.noffsets,self.runmin,self.runmax))
 
+# Retrieve Mya data for one PV and return a pandas dataframe:
 def get_mya(pv, alias, args):
-    start = time.perf_counter()
     url = 'https://epicsweb.jlab.org/myquery/interval?p=1&a=1&d=1'
     url += '&c=%s&m=%s&b=%s&e=%s'%(pv,args.m,args.start,args.end)
     if args.v>2:
@@ -150,6 +161,7 @@ def get_mya(pv, alias, args):
     requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
     retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500,502,503,504])
     session = requests.Session()
+    start = time.perf_counter()
     session.mount(url, HTTPAdapter(max_retries=retries))
     result = session.get(url, timeout=1.0, verify=False)
     df = pandas.DataFrame(result.json().get('data'))
@@ -161,8 +173,8 @@ def get_mya(pv, alias, args):
             print(df)
     return df
 
-def analyze(df, args):
-    import types
+# Calculate Faraday cup offset, collect energy/stopper values:
+def analyze(df, runmin, runmax, args):
     start_time = None
     offsets,fcups = [],[]
     if args.v>2:
@@ -182,11 +194,12 @@ def analyze(df, args):
                 for k,v in fcups:
                     if k < x[1].t - 1000*bpm_veto_seconds[1]:
                         offsets.append(v)
-    ret = FcupTable()
+    ret = FcupTable(runmin, runmax)
     ret.noffsets = len(offsets)
     ret.energies = list(set(list(df[df.energy.notnull()].energy)))
     ret.stops = list(set(list(df[df.stop.notnull()].stop)))
-    if ret.noffsets > 0:
+    ret.check()
+    if len(offsets) > 0:
         import functools
         if args.v>1:
             print('Offsets:  '+' '.join([str(x) for x in offsets]))
@@ -194,6 +207,7 @@ def analyze(df, args):
         ret.offset_rms = math.sqrt(sum([ math.pow(x-ret.offset,2) for x in offsets]) / len(offsets))
     return ret
 
+# Spawn Mya query for each PV, return analyzed FcupTable:
 def process(args, runmin, runmax):
     ret = FcupTable()
     span = args.db.get_timespan(runmin, runmax)
@@ -211,8 +225,8 @@ def process(args, runmin, runmax):
             if args.v>0:
                 print('Reading %s from Mya ...'%pv)
             futures.append(executor.submit(get_mya,pv,alias,args))
-        concurrent.futures.wait(futures)
         for f in futures:
+            concurrent.futures.wait([f])
             if f.exception() is not None:
                 print(f.exception())
                 sys.exit(222)
@@ -224,32 +238,20 @@ def process(args, runmin, runmax):
     df = pandas.concat(dfs).sort_values('t')
     if args.v>0:
         print('Analyzing data ...')
-        if args.v>1:  print(df)
+        if args.v>1:
+            print(df)
     start = time.perf_counter()
-    ret = analyze(df, args)
-    ret.runmin = runmin
-    ret.runmax = runmax
+    ret = analyze(df, runmin, runmax, args)
     ret.span = span
+    ret.set_atten()
     if args.v>0:
         print('Analyzing data took %.1f seconds.'%(time.perf_counter()-start))
-        print('Found %d fcup offset points with an average of %s.'%
-        (ret.noffsets,str(ret.offset)))
-    if len(ret.energies) > 1:
-        print('ERROR:  found multiple beam energies for %d:  %s'%(runmin,' '.join([str(x) for x in ret.energies])))
-        return ret
-    if len(ret.stops) > 1:
-        print('ERROR:  found multiple beam stopper positions for %d:  %s'%(runmin,' '.join([str(x) for x in ret.stops])))
-        return ret
-    if ret.noffsets < 5:
-        print('ERROR:  only %d points found for fcup offset for %d-%d.'%(ret.noffsets,runmin,runmax))
-        return ret
-    elif ret.noffsets < 10:
-        print('WARNING:  only %d points found for fcup offset for %d-%d.'%(ret.noffsets,runmin,runmax))
-    ret.set_atten(runmin, ret.stops[0], ret.energies[0])
-    if ret.status() and args.v>0:
-        print(ret)
+        print('Found %d fcup offset points with an average of %s.'%(ret.noffsets,str(ret.offset)))
+        if ret.status():
+            print(ret)
     return ret
 
+# Plot stuff from the generated "view" file:
 def plot(path):
     import pandas as pd
     data = pd.read_csv(path,sep='\s+',header=None)
@@ -265,6 +267,39 @@ def plot(path):
     ax2.plot(data[0], data[4],'r',marker='.',linestyle='')
     print('Close plot window to quit.')
     plt.show()
+
+# Print information, save tables for uploading to CCDB, generate "view" file:
+def closeout(runs, args):
+    sep = '\n'+':'*40
+    ngood = len(list(filter(lambda x : x[1].status(), runs)))
+    nbad = len(list(filter(lambda x : not x[1].status(), runs)))
+    if ngood > 0:
+        print(sep+'\n: Runs Calibrated (%d)'%ngood+sep)
+        [ print(r[0],':',r[1].pretty()) for r in filter(lambda x : x[1].status(), runs) ]
+        if not args.d:
+            print(sep+'\n: To Upload'+sep)
+            print('1. set CCDB_CONNECTION for clas12writer')
+            print('2. cd %s\n3. chmod +x upload\n4. ./upload'%out_dir)
+    if nbad > 0:
+        print(sep+'\n: Runs with Errors (%d)'%nbad+sep)
+        for r in filter(lambda x : not x[1].status(), runs):
+            print(r[0],'[%s minutes, %s events]'%(r[1].minutes(),str(args.db.get_event_count(r[0]))),
+                'https://clasweb.jlab.org/rcdb/runs/info/'+r[0].split('-').pop(0))
+    if not args.d:
+        print('\nLogs saved to '+tee.name)
+        if ngood > 0:
+            os.makedirs(out_dir)
+            with open('%s/upload'%out_dir,'w') as f:
+                for run in filter(lambda x : x[1].status(), runs):
+                    run[1].save()
+                    f.write(run[1].get_cmd()+'\n')
+            with open('%s/view'%out_dir,'w') as f:
+                for run in filter(lambda x : x[1].status(), runs):
+                    f.write('%d %.2f %.2f %.2f %.5f\n'%(run[1].runmin,run[1].slope,run[1].offset,run[1].offset_rms,run[1].atten))
+            print('CCDB tables written to %s'%out_dir)
+            print('Table for plotting written to %s/view\n'%out_dir)
+            if args.g:
+                plot('%s/view'%out_dir)
 
 if __name__ == '__main__':
 
@@ -287,7 +322,7 @@ if __name__ == '__main__':
         tee = Tee('./fcup-calib_%s.log'%timestamp.replace(' ','_').replace('/','-'),'w')
 
     try:
-        if len(args.runs)>1 or args.runs[0].isdigit():
+        if args.runs[0].isdigit():
             # it's a space-separated list of runs
             args.runs = sorted([ int(x) for x in args.runs ])
             args.min = args.runs[0]
@@ -306,11 +341,12 @@ if __name__ == '__main__':
         else:
             raise ValueError()
     except (ValueError,TypeError):
-        cli.error('Invalid "runs" argument:  '+str(args.runs))
+        cli.error('invalid "runs" argument:  '+str(args.runs))
 
-    args.db = RCDB()
     import atexit
+    args.db = RCDB()
     atexit.register(args.db.cleanup)
+
     runs = []
 
     if args.s:
@@ -320,7 +356,6 @@ if __name__ == '__main__':
         # one calibration per run from user-specified list:
         for run in args.runs:
             runs.append((str(run), process(args, run, run)))
-            run = args.db.db.get_next_run(run).number
     else:
         # one calibration per run in RCDB: 
         run = args.min
@@ -328,34 +363,5 @@ if __name__ == '__main__':
             runs.append((str(run), process(args, run, run)))
             run = args.db.db.get_next_run(run).number
 
-    # print a bunch of stuff:
-    sep = '\n'+':'*40
-    ngood = len(list(filter(lambda x : x[1].status(), runs)))
-    nbad = len(list(filter(lambda x : not x[1].status(), runs)))
-    if ngood > 0:
-        print(sep+'\n: Runs Calibrated (%d)'%ngood+sep)
-        [ print(r[0],':',r[1].pretty()) for r in filter(lambda x : x[1].status(), runs) ]
-        if not args.d:
-            print(sep+'\n: To Upload'+sep)
-            print('1. set CCDB_CONNECTION for clas12writer')
-            print('2. cd %s\n3. chmod +x upload\n4. ./upload'%out_dir)
-    if nbad > 0:
-        print(sep+'\n: Runs with Errors (%d)'%nbad+sep)
-        for r in filter(lambda x : not x[1].status(), runs):
-            print(r[0],'[%s minutes]'%(r[1].minutes()),'https://clasweb.jlab.org/rcdb/runs/info/'+r[0].split('-').pop(0))
-    if not args.d:
-        print('\nLogs saved to '+tee.name)
-        if ngood > 0:
-            os.makedirs(out_dir)
-            with open('%s/upload'%out_dir,'w') as f:
-                for run in filter(lambda x : x[1].status(), runs):
-                    run[1].save()
-                    f.write(run[1].get_cmd()+'\n')
-            with open('%s/view'%out_dir,'w') as f:
-                for run in filter(lambda x : x[1].status(), runs):
-                    f.write('%d %.2f %.2f %.2f %.5f\n'%(run[1].runmin,run[1].slope,run[1].offset,run[1].offset_rms,run[1].atten))
-            print('CCDB tables written to %s'%out_dir)
-            print('Table for plotting written to %s/view\n'%out_dir)
-            if args.g:
-                plot('%s/view'%out_dir)
+    closeout(runs, args)
 
