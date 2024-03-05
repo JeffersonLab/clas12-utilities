ef12d5ff6bdc45fb3f6549d2cf6688ef0846b8b8
Nathan Baltzell <baltzell@gmx.com>
2024-03-05 10:29:57 -0500

    add more run groups

--- disk-monitoring/report.sh
+++ disk-monitoring/report.sh
@@ -15,7 +15,7 @@ touch log
 echo "STARTING ..." >> log
 date >> log
 
-for xx in rg-a rg-b rg-k rg-f rg-m rg-c users
+for xx in rg-a rg-b rg-c rg-d rg-e rg-f rg-k rg-m users
 do
     indir=$DISK
     if [ $xx == "users" ]
