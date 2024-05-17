c02ebfaa56de0275fb2b0da3c7b2ec643e1ed5d7
Nathan Baltzell <baltzell@gmx.com>
2024-05-17 14:27:04 -0400

    add ccdb/rcdb snapshot cronjobs

---
+++ sqlite/ccdb.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# setup environment:
+top=/group/clas12/packages
+source /scigroup/cvmfs/hallb/clas12/sw/setup.sh
+module load clas12
+
+# define snapshot paths:
+snapshotDir=/group/clas12/packages/local/share/ccdb/sqlite
+snapshot=$snapshotDir/ccdb_`date +%F`.sqlite
+webDir=/group/clas/www/clasweb/html/clas12offline/sqlite/ccdb
+
+# generate the snapshot:
+rm -f $snapshot
+clas12-ccdb-mysql2sqlite.py $snapshot
+
+snapshotSize=$(stat -c%s $snapshot)
+
+if [ $snapshotSize == "" ] || [ $snapshotSize -lt 10 ]
+then
+  echo 'Error making ccdb snapshot.' >&2
+  rm -f $snapshot
+  exit
+fi
+
+# update link to the latest:
+cd $snapshotDir
+ln -s -f ${snapshot##*/} latest.sqlite
+rm -f LATEST ; tmp=${snapshot##*/}; tmp=${tmp%%.*}; tmp=${tmp##*_}; echo $tmp > LATEST
+cd -
+
+# delete snapshots older than 30 days:
+find $snapshotDir -maxdepth 1 -name "*.sqlite" -type f -mtime +30 -exec rm -f {} \;
+
+# rsync to web directory:
+rsync -av --delete $snapshotDir/ $webDir/
+
+# copy latest to CVMFS:
+cp -p -f -L $snapshotDir/latest.sqlite /scigroup/cvmfs/hallb/clas12/sw/noarch/data/ccdb/ccdb_latest.sqlite
+
---
+++ sqlite/rcdb.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# setup environment:
+source /scigroup/cvmfs/hallb/clas12/sw/setup.sh
+module load clas12
+
+# define snapshot paths:
+snapshotDir=/group/clas12/packages/local/share/rcdb/sqlite
+snapshot=$snapshotDir/rcdb_`date +%F`.sqlite
+webDir=/group/clas/www/clasweb/html/clas12offline/sqlite/rcdb
+
+# generate the snapshot:
+rm -f $snapshot
+clas12-rcdb-mysql2sqlite.py $snapshot
+
+snapshotSize=$(stat -c%s $snapshot)
+
+if [ $snapshotSize == "" ] || [ $snapshotSize -lt 10 ]
+then
+  echo 'Error making rcdb snapshot.' >&2
+  rm -f $snapshot
+  exit
+fi
+
+# update link to the latest:
+cd $snapshotDir
+ln -s -f ${snapshot##*/} latest.sqlite
+rm -f LATEST ; tmp=${snapshot##*/}; tmp=${tmp%%.*}; tmp=${tmp##*_}; echo $tmp > LATEST
+cd -
+
+# delete snapshots older than 7 days:
+find $snapshotDir -name "*.sqlite" -type f -mtime +7 -exec rm -f {} \;
+
+# rsync to web directory:
+rsync -av --delete $snapshotDir/ $webDir/
+
+# copy latest to CVMFS:
+cp -p -f -L $snapshotDir/latest.sqlite /scigroup/cvmfs/hallb/clas12/sw/noarch/data/rcdb/rcdb_latest.sqlite
+
