c74da1335ce154c66eedb197a77c489afcc6b246
Nathan Baltzell <baltzell@gmx.com>
2024-05-29 10:05:49 -0400

    pass args (to allow disabling duing)

--- disk/volatile_report.sh
+++ disk/volatile_report.sh
@@ -11,13 +11,13 @@ cd $HOME/disk
 
 rm -f index.html cache.html hps-volatile.html hps-cache.html
 
-$SCRIPTDIR/volatile_html.py >& index.html
+$SCRIPTDIR/volatile_html.py $@ >& index.html
 scp -q index.html clas12@ifarm1901:/group/clas/www/clasweb/html/clas12offline/disk/volatile
 
 $SCRIPTDIR/cache_html.py >& cache.html
 scp -q cache.html clas12@ifarm1901:/group/clas/www/clasweb/html/clas12offline/disk/cache/index.html
 
-$SCRIPTDIR/volatile_html.py /volatile/hallb/hps >& hps-volatile.html
+$SCRIPTDIR/volatile_html.py $@ /volatile/hallb/hps >& hps-volatile.html
 scp -q hps-volatile.html hps@ifarm1901:/group/hps/www/hpsweb/html/disk/volatile/index.html
 
 # doing it from /cache/hallb/hps doesn't work, probably need to modify query
