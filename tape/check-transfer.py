fa24a0d8e31892faea0f72a86a0f739fb6b9515c
Nathan Baltzell <baltzell@gmx.com>
2024-07-18 10:30:47 -0400

    switch to 4-space indent

--- tape/check-transfer.py
+++ tape/check-transfer.py
@@ -24,16 +24,16 @@ except re.error:
 
 import logging
 if args.v:
-  logging.basicConfig(level=logging.INFO,format='%(levelname)-9s: %(message)s')
+    logging.basicConfig(level=logging.INFO,format='%(levelname)-9s: %(message)s')
 else:
-  logging.basicConfig(level=logging.CRITICAL,format='%(levelname)-9s: %(message)s')
+    logging.basicConfig(level=logging.CRITICAL,format='%(levelname)-9s: %(message)s')
 
 logger = logging.getLogger()
 
 import os,sys
 if not os.path.isdir(args.path):
-  logger.critical('Invalid path:  '+args.path)
-  sys.exit(999)
+    logger.critical('Invalid path:  '+args.path)
+    sys.exit(999)
 
 import glob
 cached_dirs = glob.glob(args.path+'/*')
@@ -51,73 +51,73 @@ while True:
   run = db.get_prev_run(run)
 
   try:
-    try:
-      run_start_time = db.get_condition(run, 'run_start_time').value
-      event_count = db.get_condition(run, 'event_count').value
-      evio_files_count = db.get_condition(run, 'evio_files_count').value
-      age_hours_start = (datetime.datetime.now() - run_start_time).total_seconds() / 60 / 60
-    except (AttributeError,TypeError):
-      logger.warning('Run %d ignored due to invalid RCDB parameters.'%run.number)
-      continue
-    if run.number < args.m:
-      break
+      try:
+          run_start_time = db.get_condition(run, 'run_start_time').value
+          event_count = db.get_condition(run, 'event_count').value
+          evio_files_count = db.get_condition(run, 'evio_files_count').value
+          age_hours_start = (datetime.datetime.now() - run_start_time).total_seconds() / 60 / 60
+      except (AttributeError,TypeError):
+          logger.warning('Run %d ignored due to invalid RCDB parameters.'%run.number)
+          continue
+      if run.number < args.m:
+          break
   except (AttributeError,TypeError):
-    logger.warning('Run %s ignored due to invalid RCDB parameters.'%run)
-    continue
+      logger.warning('Run %s ignored due to invalid RCDB parameters.'%run)
+      continue
 
   try:
-    run_end_time = db.get_condition(run, 'run_end_time').value
-    age_hours_end = (datetime.datetime.now() - run_end_time).total_seconds() / 60 / 60
+      run_end_time = db.get_condition(run, 'run_end_time').value
+      age_hours_end = (datetime.datetime.now() - run_end_time).total_seconds() / 60 / 60
   except AttributeError:
-    run_end_time = None
-    age_hours_end = None
+      run_end_time = None
+      age_hours_end = None
 
   if run.number in args.i:
-    logger.warning('Run %d ignored due to whitelisting.'%run.number)
-    continue
+      logger.warning('Run %d ignored due to whitelisting.'%run.number)
+      continue
 
   if event_count < args.n:
-    logger.warning('Run %d ignored due to small number of events.'%run.number)
-    continue
+      logger.warning('Run %d ignored due to small number of events.'%run.number)
+      continue
 
   if evio_files_count < args.f:
-    logger.warning('Run %d ignored due to small number of files.'%run.number)
-    continue
+      logger.warning('Run %d ignored due to small number of files.'%run.number)
+      continue
 
   if age_hours_start < args.e:
-    logger.warning('Run %d ignored due to start time less than %.1f hours ago.'%(run.number,args.e))
-    continue
+      logger.warning('Run %d ignored due to start time less than %.1f hours ago.'%(run.number,args.e))
+      continue
 
   run_dir = list(filter(lambda d: re.match(args.R,d).group(1).lstrip('0')==str(run.number) , cached_dirs))
 
   if len(run_dir) == 0:
-    logger.critical('Run %d started more than %.1f hours ago in RCDB but missing /mss directory.'%(run.number,args.e))
-    error_runs.append(run.number)
-    continue
+      logger.critical('Run %d started more than %.1f hours ago in RCDB but missing /mss directory.'%(run.number,args.e))
+      error_runs.append(run.number)
+      continue
   elif len(run_dir) > 1:
-    logger.critical('Run %d has multiple /mss directories.'%(run.number))
-    error_runs.append(run.number)
-    continue
+      logger.critical('Run %d has multiple /mss directories.'%(run.number))
+      error_runs.append(run.number)
+      continue
 
   run_dir = run_dir.pop(0)
 
   if age_hours_end is None:
-    logger.info('Run %d ignored due to missing end time in RCDB'%run.number)
-    continue
+      logger.info('Run %d ignored due to missing end time in RCDB'%run.number)
+      continue
 
   if age_hours_end > args.c:
-    if len(glob.glob(run_dir+'/*')) < evio_files_count:
-      logger.critical('Run %d ended more than %.1f hours ago in RCDB but missing /mss files.'%(run.number,args.c))
-      error_runs.append(run.number)
-      continue
-    else:
-      logger.info('Run %d ran from %s to %s and complete at /mss.'%(run.number,run_start_time,run_end_time))
+      if len(glob.glob(run_dir+'/*')) < evio_files_count:
+          logger.critical('Run %d ended more than %.1f hours ago in RCDB but missing /mss files.'%(run.number,args.c))
+          error_runs.append(run.number)
+          continue
+      else:
+          logger.info('Run %d ran from %s to %s and complete at /mss.'%(run.number,run_start_time,run_end_time))
 
   else:
-    logger.info('Run %d ignored due to end time less than %.1f hours ago.'%(run.number,args.c))
+      logger.info('Run %d ignored due to end time less than %.1f hours ago.'%(run.number,args.c))
 
   if age_hours_start > args.d*24:
-    break
+      break
 
 sys.exit(len(error_runs))
 
