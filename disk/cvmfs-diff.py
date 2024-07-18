2188617ac754998a49f475cca45e2f4cb11e05b2
Nathan Baltzell <baltzell@gmx.com>
2024-07-18 10:50:11 -0400

    treat symbolic links specially (#7)

--- disk/cvmfs-diff.py
+++ disk/cvmfs-diff.py
@@ -14,7 +14,8 @@
 # of sync with CVMFS until the affected files are modified again.
 #
 # Note, permissions changes do not (always?) propagate, need to remove the file, let
-# it propagate, then add it back with correct permissions.
+# it propagate, then add it back with correct permissions.  The rsync above does
+# detect and sync a permissions change, so the culprit must be downstream.
 #
 # Note, some CVMFS attributes prevent using standard filesystem tools:
 # * sticky bit doesn't exist (it's read-only)
@@ -25,18 +26,16 @@
 
 default_src = '/scigroup/cvmfs/hallb/clas12/sw'
 default_dest = '/cvmfs/oasis.opensciencegrid.org/jlab/hallb/clas12/sw'
-
-ignores = ['.*/rgm_fall2021-ai.yaml$','.*/\.git/.*','.*/gibuu/.*']
+default_ignores = ['.*/\.git/.*']
 
 import argparse
 cli = argparse.ArgumentParser('cvmfs-diff',description='Compare local source and CVMFS target filesystems.',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 cli.add_argument('-l',help='local source directory (or subpath underneath default)',metavar='PATH',default=default_src)
 cli.add_argument('-c',help='CVMFS target directory',metavar='PATH',default=None)
-cli.add_argument('-i',help='ignore path regex, repeatable',default=[],action='append')
+cli.add_argument('-i',help='ignore path regex, repeatable',default=default_ignores,action='append')
 cli.add_argument('-v',help='verbose mode, repeatable',action='count',default=0)
 cli.add_argument('-t',help='check timestamps',action='store_true',default=False)
 args = cli.parse_args()
-ignores.extend(args.i)
 
 import logging
 if args.v>1:
@@ -61,29 +60,49 @@ if not os.path.isdir(args.c):
 if not args.c.startswith('/cvmfs/'):
     logging.warning('Destination does not start with "/cvmfs".')
 
+def difflink(a,b):
+    # ignore dead links on the source filesystem:
+    if os.path.exists(os.path.realpath(a)):
+        # if it's an absolute symlink, ignore prefix:
+        if os.path.isabs(os.readlink(a)):
+            if os.path.realpath(a) != os.path.realpath(b):
+                if os.path.realpath(a)[len(args.l):] != os.path.realpath(b)[len(args.c):]:
+                    logging.getLogger().warning('%s %s'%(os.path.realpath(a)[len(args.l):],os.path.realpath(b)[len(args.c):]))
+                    logging.getLogger().warning('%s %s'%(os.path.realpath(a),os.path.realpath(b)))
+                    logging.getLogger().warning('ALNK: %s'%a)
+                    return False
+        # if it's a relative symlink, they must be equal:
+        elif os.readlink(a) != os.readlink(b):
+            logging.getLogger().warning('RLNK: %s'%a)
+            return Falsr
+    return True
+
 def diff(a,b):
+    if os.path.islink(a):
+        return difflink(a,b)
     if not os.path.exists(b):
         logging.getLogger().warning('MISS: %s'%a)
         return False
-    s1 = os.stat(a)
-    s2 = os.stat(b)
-    # require equal file sizes:
-    if not os.path.isdir(a) and s1.st_size != s2.st_size:
-        logging.getLogger().warning('SIZE: %s (%d!=%d)'%(a,s1.st_size,s2.st_size))
-        return False
-    # require equal world permissions bits:
-    if (s1.st_mode&0xF) != (s2.st_mode&0xF):
-        logging.getLogger().warning('MODE: %s (%d!=%d)'%(a,s1.st_mode,s2.st_mode))
-        return False
-    # require consistent modification times:
-    if args.t and int(s1.st_mtime) > int(s2.st_mtime):
-        logging.getLogger().warning('TIME: %s (%d!=%d)'%(a,s1.st_mtime,s2.st_mtime))
-        return False
+    else:
+        s1 = os.stat(a)
+        s2 = os.stat(b)
+        # require equal file sizes:
+        if not os.path.isdir(a) and s1.st_size != s2.st_size:
+            logging.getLogger().warning('SIZE: %s (%d!=%d)'%(a,s1.st_size,s2.st_size))
+            return False
+        # require equal world permissions bits:
+        if (s1.st_mode&0xF) != (s2.st_mode&0xF):
+            logging.getLogger().warning('MODE: %s (%d!=%d)'%(a,s1.st_mode,s2.st_mode))
+            return False
+        # require consistent modification times:
+        if args.t and int(s1.st_mtime) > int(s2.st_mtime):
+            logging.getLogger().warning('TIME: %s (%d!=%d)'%(a,s1.st_mtime,s2.st_mtime))
+            return False
     return True
 
 def ignore(path):
     import re
-    for x in ignores:
+    for x in args.i:
         if re.match(x,path):
             return True
     return False
