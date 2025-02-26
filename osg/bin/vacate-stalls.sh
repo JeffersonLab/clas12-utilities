aab555a3ce9bbd4becbbf48942a1a78a80c24cc2
Nathan Baltzell <baltzell@gmx.com>
2025-02-26 16:19:14 -0500

    add hours argument

--- osg/bin/vacate-stalls.sh
+++ osg/bin/vacate-stalls.sh
@@ -4,22 +4,23 @@ dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 cache=$HOME/vacate-stalls.txt
 
-limit=26
+limit=$1
 
 touch $cache
 
 $dirname/condor-probe.py -vacate $limit >> $cache
 
-if ! [ -z $1 ]; then
-    tmp=$(mktemp /tmp/gemc/vacate.XXXXXX)
-    # 1. remove username, keeping only site, host, and job ids
-    # 2. sort and remove uniques, i.e. same host and job id
-    # 3. count per host and sort by counts
-    sed 's/@/ /g' $cache | awk '{print$1,$(NF-1),$NF}' | sort | uniq | awk '{print$1,$2}' | sort | uniq -c | sort -n -r > $tmp
-    if [ $(cat $tmp | wc -l) -ne 0 ]; then
-        echo "Vacated jobs longer than $limit hours in the past 24 hours:"
-        cat $tmp
-    fi
-    rm -f $cache $tmp
-fi
+# this is done in daily-digest now ...
+#if ! [ -z $1 ]; then
+#    tmp=$(mktemp /tmp/$USER/vacate.XXXXXX)
+#    # 1. remove username, keeping only site, host, and job ids
+#    # 2. sort and remove uniques, i.e. same host and job id
+#    # 3. count per host and sort by counts
+#    sed 's/@/ /g' $cache | awk '{print$1,$(NF-1),$NF}' | sort | uniq | awk '{print$1,$2}' | sort | uniq -c | sort -n -r > $tmp
+#    if [ $(cat $tmp | wc -l) -ne 0 ]; then
+#        echo "Vacated jobs longer than $limit hours in the past 24 hours:"
+#        cat $tmp
+#    fi
+#    rm -f $cache $tmp
+#fi
 
